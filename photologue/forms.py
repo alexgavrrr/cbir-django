ab594cc532e72d66ab68012755a54de54118bc9a
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-04-14 03:52:49 +0300

    widget to load multiple files

--- photologue/forms.py
+++ photologue/forms.py
@@ -6,23 +6,17 @@ from .models import Database, DatabasePhoto
 
 FILE_MAX_LENGTH = 100
 
-
+file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
 class DatabaseForm(ModelForm):
     lol_field = forms.CharField(required=False,
                                 label='Label for lol field',
                                 initial='lol-initial')
     photos = forms.ImageField(required=False,
-                              label='photos',)
+                              label='photos',
+                              widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
     class Meta:
         model = Database
         fields = ['date_added', 'title', 'slug', 'description', 'description_file']
         labels = {'description_file': 'Description file'}
-
-
-class ImageForm(forms.ModelForm):
-    image = forms.ImageField(label='Image')
-    class Meta:
-        model = DatabasePhoto
-        fields = ('image', )
