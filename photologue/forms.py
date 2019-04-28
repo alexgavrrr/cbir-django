acb6041888e28490185cf86d80253a5c2637f86a
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-04-28 19:48:47 +0300

    added zip field to a database form

--- photologue/forms.py
+++ photologue/forms.py
@@ -1,3 +1,6 @@
+import zipfile
+from zipfile import BadZipFile
+
 from django import forms
 from django.forms import ModelForm
 
@@ -15,10 +18,28 @@ class DatabaseForm(ModelForm):
     photos = forms.ImageField(required=False,
                               label='photos',
                               widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    zip_file = forms.FileField()
 
     class Meta:
         model = Database
         fields = ['date_added', 'title', 'slug', 'description']
+
+    def clean_zip_file(self):
+        """Open the zip file a first time, to check that it is a valid zip archive.
+        We'll open it again in a moment, so we have some duplication, but let's focus
+        on keeping the code easier to read!
+        """
+        zip_file = self.cleaned_data['zip_file']
+        try:
+            zip = zipfile.ZipFile(zip_file)
+        except BadZipFile as e:
+            raise forms.ValidationError(str(e))
+        bad_file = zip.testzip()
+        if bad_file:
+            zip.close()
+            raise forms.ValidationError('"%s" in the .zip archive is corrupt.' % bad_file)
+        zip.close()  # Close file in all cases.
+        return zip_file
 
 
 class EventForm(ModelForm):
