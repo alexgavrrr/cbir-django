cd65808e911e03d69ee751219f2a24362028e6e9
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-05-01 05:26:57 +0300

    removed redundant commands from commands and added new for evaluating

--- cbir/commands.py
+++ cbir/commands.py
@@ -10,15 +10,6 @@ from cbir.legacy_utils import draw_result
 import cbir_evaluation.start_evaluation
 
 
-def prepare_directory_structure(args):
-    persistent_state = args.persistent_state or cbir.PERSISTENT_STATE
-    databases = persistent_state / 'databases'
-    if not os.path.exists(persistent_state):
-        os.mkdir(persistent_state)
-    if not os.path.exists(databases):
-        os.mkdir(databases)
-
-
 def _prepare_place_for_database(database):
     os.mkdir(Path(DATABASES) / database)
 
@@ -39,131 +30,64 @@ def _get_registered_cbir_indexes_of_database(database):
 
 ####################
 
-
-def build_cbir_index(database,
-                     cbir_index_name,
-                     path_to_images_to_index,
-                     path_to_images_to_train_clusterer=None):
-    """
-    :param database:
-    :param cbir_index_name
-    :param path_to_images_to_index:
-    :param path_to_images_to_train_clusterer:
-    """
-    path_to_images_to_train_clusterer = path_to_images_to_train_clusterer or path_to_images_to_index
-
-    registered_databases = _get_registered_databases()
-    if database not in registered_databases:
-        _prepare_place_for_database(database)
-
-    cbir_indexes_of_database = _get_registered_cbir_indexes_of_database(database)
-    if cbir_index_name in cbir_indexes_of_database:
-        message = f'Cbir index {cbir_index_name} for database {database} already exists.'
-        raise ValueError(message)
-
-    if not os.path.isdir(path_to_images_to_index):
-        message = (f'Path to directory containing images must be provided.'
-                   f'But {path_to_images_to_index} provided is not a directory')
-        raise ValueError(message)
-
-    os.mkdir(Path(DATABASES) / database / cbir_index_name)
-    storage = cbir_core.Storage(Path(DATABASES) / database / cbir_index_name,
-                                testing_path=path_to_images_to_index,
-                                training_path=path_to_images_to_train_clusterer,
-                                des_type='l2net',
-                                label='',
-                                max_keypoints=2000,
-                                L=2,
-                                K=10,
-                                extensions=['jpg'],
-                                debug=True)
+def prepare_cbir_directory_structure(persistent_state, **kwargs):
+    persistent_state = persistent_state or cbir.PERSISTENT_STATE
+    databases = persistent_state / 'databases'
+    if not os.path.exists(persistent_state):
+        os.mkdir(persistent_state)
+    if not os.path.exists(databases):
+        os.mkdir(databases)
 
 
-def search(database,
-           cbir_index_name,
-           query,
-           debug=False,
-           **kwargs):
-    """
-    :param database:
-    :param cbir_index_name:
-    :param query:
-    """
-    registered_databases = _get_registered_databases()
-    if database not in registered_databases:
-        message = f'Database {database} has not been registered yet.'
-        raise ValueError(message)
+def _init_dirs(train_dir, test_dir, gt_dir, is_sample):
+    exists_none = False
+    exists_not_none = False
+    for now in [train_dir, test_dir, gt_dir]:
+        exists_none |= now is None
+        exists_not_none |= now is not None
+    if exists_none and exists_not_none:
+        raise ValueError
 
-    cbir_indexes_of_database = _get_registered_cbir_indexes_of_database(database)
-    if cbir_index_name not in cbir_indexes_of_database:
-        message = f'Cbir index {cbir_index_name} for database {database} has not been created yet.'
-        raise ValueError(message)
+    if exists_none:
+        suffix = "_sample" if is_sample else ""
+        paris = 'Paris' + suffix
+        oxford = 'Oxford' + suffix
+        data_building_root = str(Path(cbir.BASE_DIR) / 'data' / 'Buildings' / 'Original')
+        train_dir = str(Path(data_building_root) / paris / 'jpg')
+        test_dir = str(Path(data_building_root) / oxford / 'jpg')
+        gt_dir = str(Path(data_building_root) / oxford / 'gt')
 
-    # TODO: Make more robust loading of a storage object.
-    # Now it seems like it can begin to build new Storage.
-    storage = cbir_core.Storage(Path(DATABASES) / database / cbir_index_name,
-                                testing_path=None,
-                                training_path=None,
-                                des_type='l2net',
-                                label='',
-                                max_keypoints=2000,
-                                L=2,
-                                K=10,
-                                extensions=['jpg'],
-                                debug=True)
+    return train_dir, test_dir, gt_dir
 
-    if not query:
-        raise ValueError('No query provided')
-    if not os.path.exists(query):
-        message = f'File {query} does not exist'
-        raise ValueError(message)
-    if not basic.is_image(query):
-        message = f'File {query} is not an image'
-        raise ValueError(message)
+def evaluate_with_all_descriptors(
+        train_dir,
+        test_dir,
+        gt_dir,
+        is_sample,
+        **kwargs):
 
-    similar = storage.get_similar(query, topk=5,
-                                  n_candidates=50, debug=debug,
-                                  qe_enable=False)
-    images = [v[1] for v in list(zip(*similar))[0]]
-
-    if debug:
-        print(f'Result images: {images}')
-        draw_result(query, images)
-
-    return images
+    train_dir, test_dir, gt_dir = _init_dirs(train_dir, test_dir, gt_dir, is_sample)
+    cbir_evaluation.start_evaluation.start_train_test_all_descriptors_and_modes(
+        train_dir,
+        test_dir,
+        gt_dir)
 
 
-def compute_descriptors(database,
-                        cbir_index_name,
-                        list_paths_to_images_to_compute_descriptors_for,
-                        directory_or_list):
+def evaluate(
+        train_dir,
+        test_dir,
+        gt_dir,
+        is_sample,
+        des_type,
+        sv,
+        qe,
+        **kwargs):
 
-    options = ['directory', 'list']
-    if directory_or_list not in options:
-        raise ValueError(f'Provided {directory_or_list} not in list of possible options')
-    raise NotImplemented
-
-
-def build_index_and_inverted_index(database,
-                                   cbir_index_name,
-                                   path_to_images_to_index,
-                                   path_to_images_to_train_clusterer=None):
-    raise NotImplemented
-
-
-def add_image(database,
-              cbir_index_name,
-              path_to_image_to_add):
-    raise NotImplemented
-
-
-def add_images(database,
-               cbir_index_name,
-               list_paths_to_images_to_add):
-    raise NotImplemented
-
-
-
-def evaluate(args):
-    cbir_evaluation.start_evaluation.do_train_test(args.train_dir, args.test_dir, args.gt_dir,
-                                                   args.sample)
+    train_dir, test_dir, gt_dir = _init_dirs(train_dir, test_dir, gt_dir, is_sample)
+    cbir_evaluation.start_evaluation.start_train_test(
+        train_dir,
+        test_dir,
+        gt_dir,
+        des_type=des_type,
+        sv=sv,
+        qe=qe)
