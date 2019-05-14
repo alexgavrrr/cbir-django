0fcedf6b85bb955e25b1dbe6e047cf4ae13b6d09
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-05-15 02:26:23 +0300

    updated a little download_flickr

--- data/Buildings/Original/Oxford/jpg/distractor/download_flickr_100k.sh
+++ data/Buildings/Original/Oxford/jpg/distractor/download_flickr_100k.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # for i in `seq 1 40`; do
-for i in `seq 1 40`; do
+for i in `seq $1 $2`; do
     printf "Downloading %.2d out of 40\n" $i;
     partNum=$(printf "%.2d" $i)
     cmd='wget http://www.robots.ox.ac.uk/~vgg/data/oxbuildings/ox100k/oxc1_100k.part'$partNum'.rar';
