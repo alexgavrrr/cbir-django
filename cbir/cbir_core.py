b551568efd806176f9edbabda3a294df6a7cf689
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-05-15 18:52:36 +0300

    added little logging

--- cbir/cbir_core.py
+++ cbir/cbir_core.py
@@ -483,6 +483,7 @@ class CBIRCore:
         start = time.time()
         candidates_raw = self.get_candidates_raw(img_bovw[:-1])
         if len(candidates_raw) == 0:
+            logger.warning('0 candidates if filter most frequent. Trying without filtering')
             candidates_raw = self.get_candidates_raw(img_bovw[:-1], filter=False)
         candidates = set()
         for candidate_raw in candidates_raw:
