77637851c0847406d6819f53cbb469d35b89a368
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-05-28 18:28:21 +0300

    removed database prefix in urls for indexes

--- photologue/urls.py
+++ photologue/urls.py
@@ -15,16 +15,16 @@ urlpatterns = [
 
     # Info about indexes for concrete database. If no database parameter provided then show list of databases
     # parameter ?database=slug
-    path('database/index/info/', views.database_index_info_view, name='database_index_info'),
+    path('index/info/', views.database_index_info_view, name='database_index_info'),
 
     # Form to create new index for some database without deleting old ones.
     # Must provide option whether to use new index as default.
     # parameter ?database=slug
-    path('database/index/create/', views.database_index_create_view, name='database_index_create'),
+    path('index/create/', views.database_index_create_view, name='database_index_create'),
 
     # Info about concrete index: which database, how many objects indexed,
     # list of events which used this index...
-    path('database/index/<slug:slug>/', views.database_index_detail_view, name='database_index_detail'),
+    path('index/<slug:slug>/', views.database_index_detail_view, name='database_index_detail'),
 
     path('event/', views.event_list_view, name='event_list'),
     path('event/create/', views.event_create_view, name='event_create'),
