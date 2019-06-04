ae8cd5498a9644d70a041e4ec1808b54bcf1b8ca
Alexey Gavrikov <alex.gavr97@gmail.com>
2019-06-04 14:48:46 +0300

    pre cache d-paris i-paris-1

--- cbir/server.py
+++ cbir/server.py
@@ -22,6 +22,20 @@ class RPCThreadingServer(socketserver.ThreadingMixIn, SimpleXMLRPCServer):
 CBIR_CORES = defaultdict(lambda: defaultdict(lambda: None))
 
 
+def cache_index(database, index):
+    logger = logging.getLogger()
+    cbir_core = CBIRCore.get_instance(database, index)
+    start = time.time()
+    logger.info(f'Loading fd, ca, bow, inv for {database}-{index}...')
+    cbir_core.set_fd(cbir_core.load_fd())
+    cbir_core.set_ca(cbir_core.load_ca())
+    cbir_core.set_bow(cbir_core.load_bow())
+    cbir_core.set_inv(cbir_core.load_inv())
+    CBIR_CORES[database][index] = cbir_core
+    time_loading = round(time.time() - start, 3)
+    logger.info(f'Loaded fd, ca, bow, inv for {time_loading} sec.')
+
+
 def run(port, nthreads):
     with RPCThreadingServer(('localhost', port),
                             requestHandler=RequestHandler) as server:
@@ -31,24 +45,21 @@ def run(port, nthreads):
         @server.register_function
         def search(database, index, query, search_params):
             logger = logging.getLogger()
-            cbir_core = CBIR_CORES[database][index]
-            if cbir_core is None:
-                cbir_core = CBIRCore.get_instance(database, index)
-
-                start = time.time()
-                logger.info(f'Loading fd, ca, bow, inv for {database}-{index}...')
-                cbir_core.set_fd(cbir_core.load_fd())
-                cbir_core.set_ca(cbir_core.load_ca())
-                cbir_core.set_bow(cbir_core.load_bow())
-                cbir_core.set_inv(cbir_core.load_inv())
-                CBIR_CORES[database][index] = cbir_core
-                time_loading = round(time.time() - start, 3)
-                logger.info(f'Loaded fd, ca, bow, inv for {time_loading} sec.')
+            if CBIR_CORES[database][index] is None:
+                cache_index(database, index)
             else:
                 logger.info(f'cbir_core {database}-{index} already loaded')
-
+            cbir_core = CBIR_CORES[database][index]
             result = cbir_core.search(query, **search_params)
             return result
+
+        logger = logging.getLogger()
+        database_pre_cache = 'd-paris'
+        index_pre_cache = 'i-paris-1'
+        try:
+            cache_index(database_pre_cache, index_pre_cache)
+        except ValueError:
+            logger.warning(f'Index for pre-caching {database_pre_cache}-{index_pre_cache} does not exists')
 
         print(f'Listening on port {port}')
         try:
